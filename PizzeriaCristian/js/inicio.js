9276a43c32b461a14bcecf215ac6ce192bd2869e
Cristian Daniel Villalba <villalba.macri24@triana.salesianos.edu>
2025-02-19 15:02:02 +0100

    paginaPizza

--- PizzeriaCristian/js/inicio.js
+++ PizzeriaCristian/js/inicio.js
@@ -1,6 +1,10 @@
-document.querySelectorAll('nav a').forEach(link =>{
-    link.addEventListener('click', function(){
-        document.querySelector('nav a.active').classList.remove('active');
-        this.classList.add('active');
-    })
-})
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelectorAll('nav a').forEach((link) =>{
+        link.addEventListener('click', function(){
+            document.querySelector('nav a.active').classList.remove('active');
+            this.classList.add('active');
+        });
+    });
+});
+
+document.querySelectorAll("bi bi-list")
