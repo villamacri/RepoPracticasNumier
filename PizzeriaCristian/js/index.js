c14c939342c5cae4ef420164dd9f7920df7c539e
Cristian Daniel Villalba <villalba.macri24@triana.salesianos.edu>
2025-02-21 14:51:41 +0100

    paginaPizza

--- PizzeriaCristian/js/index.js
+++ PizzeriaCristian/js/index.js
@@ -1,8 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("language a").forEach((link) => {
+  document.querySelectorAll(".language a.ingles img").forEach((link) => {
     link.addEventListener("click", function () {
-      document.querySelector("language a.active").classList.remove("active");
+      const activeLink = document.querySelector(".language a.español img.active");
+      if(activeLink){
+        activeLink.classList.remove("active");
+      }
       this.classList.add("active");
+      const languageButton = document.getElementById("language-button");
+      languageButton.textContent = 'See menu';
+    });
+  });
+  document.querySelectorAll(".language a.español img").forEach((link) => {
+    link.addEventListener("click", function () {
+      const activeLink = document.querySelector(".language a.ingles img.active");
+      if(activeLink){
+        activeLink.classList.remove("active");
+        
+      }
+      this.classList.add("active");
+      const languageButton= document.getElementById("language-button");
+      languageButton.textContent = 'Ver carta';
     });
   });
 });
--- PizzeriaCristian/js/inicio.js
+++ PizzeriaCristian/js/inicio.js
@@ -5,18 +5,27 @@ document.addEventListener("DOMContentLoaded", function () {
       this.classList.add("active");
     });
   });
-});
 
-const menuToggle = document.getElementById("menu-toggle");
-const sidebar = document.querySelector(".sidebar");
-const overlay = document.querySelector(".overlay");
-const columnDcha = document.querySelector(".column-dcha");
+  const menuToggle = document.getElementById("menu-toggle");
+  const sidebar = document.querySelector(".sidebar");
+  const overlay = document.querySelector(".overlay");
+  const columnDcha = document.querySelector(".column-dcha");
 
-function toggleMenu() {
+  function toggleMenu() {
     sidebar.classList.toggle("open");
     overlay.classList.toggle("visible");
     columnDcha.classList.toggle("darken"); // Agregar clase para oscurecer
-}
+  }
 
-menuToggle.addEventListener("click", toggleMenu);
-overlay.addEventListener("click", toggleMenu);
+  menuToggle.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", toggleMenu);
+
+  const submenuToggle = document.getElementById("submenu-toggle");
+  const submenu = document.querySelector(".submenu");
+
+  function toggleSubmenu() {
+    submenu.classList.toggle("open-submenu");
+    submenuToggle.querySelector(".arrow").classList.toggle("open-submenu");
+  }
+  submenuToggle.addEventListener("click", toggleSubmenu);
+});
